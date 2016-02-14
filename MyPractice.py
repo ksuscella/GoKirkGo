31c72c19cabe45713e4e150df21b2c3affc066fc
ksuscella <kirk@Kirks-MacBook-Pro.local>
2016-02-14 15:21:55 -0500

    v2 & params

--- MyPractice.py
+++ MyPractice.py
@@ -1,50 +1,20 @@
-import random
-import math
+#!/usr/bin/env python
+#############################################################################################################                                                                  
 import sys
-import time
 
-deg_scan = 180
-start_scan=10
-increm = 10
-stop_distance = 20
-tracker=0   
-situation = {}
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
 
-def servo():
-    #Run Through Scan
-    for i in xrange(start_scan,deg_scan,increm):         #loop by 20
-        situation[i] = random.randint(0,30)
-    print(situation)
 
-def decision():
-    #Step 1 - Lets see if we can go straight
-    middle = deg_scan/2 
+print sys.argv[0]
+print sys.argv[1]
+print sys.argv[1:]
+
+arguments = sys.argv[1:]
+
+print arguments[0]
+
+for a in arguments:
+    print(a)
     
-    #Should we go straight?
-    if situation[middle] > stop_distance:
-        #move forward
-        print("moving forward " + str(situation[middle]) + "cm")
-        
-    elif situation[deg_scan-increm] > stop_distance:
-        #move left?
-        print("moving left " + str(situation[deg_scan-increm]) + "cm")
-        
-    elif situation[start_scan] > stop_distance:
-        #move right?
-        print("moving right " + str(situation[start_scan])+"cm")
-    
-    else:
-        print("turning around")
 
-
-while True:
-    servo()
-    decision()
-    time.sleep(1)
-    tracker = tracker + 1
-    if tracker > 10:
-        break
-
-
-
-        
---
+++ SkyNet_v2.py
@@ -0,0 +1,116 @@
+from gopigo import *
+import random
+import math
+import sys
+import time
+
+#argument variables (pass in as args to callibrate)
+# -- --------------------------
+arg_stop_dist = 20
+arg_rot_number = 18
+arg_fwd_sleep = 1
+arg_LtRt_sleep = .8
+arg_turn_sleep = 1.6
+arg_robot_speed = 150
+arg_decisions = 10
+# -- ---------------------------
+
+full_scan = 180
+deg_scan = 140
+start_scan=40
+increm = 10
+tracker=0   
+situation = {}
+
+def servo_int():
+    #Run Through Scan
+    for servo_pos in xrange(start_scan,deg_scan,increm):
+        servo(servo_pos)
+        time.sleep(.02)
+        dist=us_dist(15)			#Find the distance of the object in front
+        situation[servo_pos] = dist
+        
+def decision():
+    #Step 1 - Lets see if we can go straight
+    middle = full_scan/2 
+    
+    #Should we go straight?
+    if situation[middle] > arg_stop_dist:
+        #move forward
+        print("moving forward " + str(situation[middle]) + "cm")
+        move_forward()
+    elif situation[deg_scan-increm] > arg_stop_dist:
+        #move left?
+        print("moving left " + str(situation[deg_scan-increm]) + "cm")
+        turn_left()
+    elif situation[start_scan] > arg_stop_dist:
+        #move right?
+        print("moving right " + str(situation[start_scan])+"cm")
+        turn_right()
+    else:
+        print("turning around")
+        turn_around()
+
+def move_forward():
+    #GoPiGo moves forward a short distance
+    enc_tgt(1,1,arg_rot_number)
+    fwd()
+    time.sleep(arg_fwd_sleep)
+    stop()
+    
+def turn_around():
+    #Turn Around
+    
+    #Pick which way to turn
+    if situation[deg_scan-increm] > situation[start_scan]:
+        left()
+    else:
+        right()
+    time.sleep(arg_turn_sleep)
+    stop()
+    
+def turn_left():
+    #Turn Left
+    left()
+    time.sleep(arg_LtRt_sleep)
+    stop()
+    
+def turn_right():
+    #Turn Right
+    right()
+    time.sleep(arg_LtRt_sleep)
+    stop()
+
+### PASS IN ARGUMENTS ###
+#1 - STOP DISTANCE
+#2 - FORWARD (ROTATIONS)
+#3 - FORWARD (SLEEP)
+#4 - LEFT OR RIGHT (SECONDS)
+#5 - TURN AROUND (SECONDS)
+#6 - SET SPEED
+#7 - NUMBER OF DECISION ITERATIONS
+
+args = sys.argv[1:] #Get Arguments
+if len(args) == 7:  #Use defaults if arguments don't match
+    #Take Arguments
+    arg_stop_dist = args[0]
+    arg_rot_number = args[1]
+    arg_fwd_sleep = args[2]
+    arg_LtRt_sleep = args[3]
+    arg_turn_sleep = args[4]
+    arg_robot_speed = args[5]
+    arg_decisions = args[6]
+
+
+print "Press ENTER to begin"
+raw_input()				#Wait for input to start
+set_speed(arg_robot_speed)
+
+while True:
+    servo_int()
+    decision()
+    time.sleep(1)
+    tracker = tracker + 1
+    if tracker > arg_decisions:
+        break
+
