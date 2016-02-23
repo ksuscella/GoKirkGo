a37fa7aab5cafb1da6dd3f560a83c0629896bc12
ksuscella <kirk@Kirks-MacBook-Pro.local>
2016-02-22 23:41:08 -0500

    lidar stuff

--- MyPractice.py
+++ MyPractice.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python
 #############################################################################################################                                                                  
 
-middle_scan = 80
+import os
+import json
+import math
 
-for servo_pos in xrange(0,40,10):
-    print(servo_pos)
+print(os.getcwd())
+os.chdir("/Users/kirk/Documents/GoPiGo/GoKirkGo/")
+print(os.getcwd())
+# Open a file
+fo = open("distance.json", "r")
+print "Name of the file: ", fo.name
+print "Closed or not : ", fo.closed
+print "Opening mode : ", fo.mode
+print "Softspace flag : ", fo.softspace
+
+parsed_json = json.loads(str(fo.read()))
+fo.close()
+
+#print(parsed_json['5'])
+
+for ang_l in xrange(0,175,5):
+    dist_l = parsed_json[str(ang_l)]
+    x=(int(dist_l*math.cos(math.pi*(ang_l)/180)))
+    y=(int(dist_l*math.sin(math.pi*(ang_l)/180)))
+    print(str(ang_l) + ", " + str(x) + ", "+str(y) + " - " + str(dist_l))
+    
