c400c5ac6b0717987b451b5a9b8b9f16cd06f95b
ksuscella <kirk@Kirks-MacBook-Pro.local>
2016-02-23 22:27:24 -0500

    expand on sensor

--- MyPractice.py
+++ MyPractice.py
@@ -5,6 +5,9 @@ import os
 import json
 import math
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 print(os.getcwd())
 os.chdir("/Users/kirk/Documents/GoPiGo/GoKirkGo/")
 print(os.getcwd())
@@ -20,9 +23,20 @@ fo.close()
 
 #print(parsed_json['5'])
 
+myX = []
+myY = []
+
 for ang_l in xrange(0,175,5):
+#for ang_l in xrange(60,105,5):
     dist_l = parsed_json[str(ang_l)]
-    x=(int(dist_l*math.cos(math.pi*(ang_l)/180)))
-    y=(int(dist_l*math.sin(math.pi*(ang_l)/180)))
-    print(str(ang_l) + ", " + str(x) + ", "+str(y) + " - " + str(dist_l))
-    
+    myX.append(int(dist_l*math.cos(math.pi*(ang_l)/180)))
+    myY.append(int(dist_l*math.sin(math.pi*(ang_l)/180)))
+    #print(str(ang_l) + ", " + str(x) + ", "+str(y) + ", " + str(dist_l))
+
+plt.plot(myX,myY)
+plt.show()
+
+#slope, intercept = np.polyfit(np.log(myX), np.log(myY), 1)
+#print(slope)
+#plt.loglog(myX, myY, 'x')
+#plt.show()
--- distance_servo_chck.py
+++ distance_servo_chck.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python
 ########################################################################                                                                  
-#
+# Test program to capture distances & experiment with grids
 ########################################################################
 from gopigo import *
 
-servo_pos=90
 print "Press ENTER to start scan"
 raw_input()				#Wait for input to start
 
-# Ok Lets Start - reset position & capture distance
-servo(servo_pos)
-dist=us_dist(15)			#Find the distance of the object in front
-print "Servo @: ",servo_pos," Distance: ",dist," cm"
-time.sleep(.5)			# Take a break in between operations. 
+mid_servo_pos=80 			# middle
+start_servo_pos = 0			# start angle
+end_servo_pos= 160 			# end angle - prevent servo damage
+increm = 1 					# degree to increment
+sample = 5 					# Lets capture more than one distance 
+lim = 250					# max distance value
 
-servo_pos = 30 				# Prevent camera installation issues
+myX = []
+myY = []
 
-while True:
-	#Start Scan
-	servo(servo_pos)
-	dist=us_dist(15)
-	print "Servo @: ",servo_pos," Distance: ",dist," cm"
-	servo_pos=servo_pos+20
-	time.sleep(.5)		# Take a break in between operations. 
-	if servo_pos>150:
-		servo(90)
-		break	
+#Loop over angles and capture results
+for a_ang in xrange(start_servo_pos,end_servo_pos+increm,increm):
+	dist_l = []
+	servo(a_ang)
+	time.sleep(.25) # Give it time to get into position
+	for a_sample in xrange(1,sample+1,1):
+		dist_l.append(us_dist(15))
+		time.sleep(.01)
 	
-	 
+	#Find the sample that is most common among all the samples for a particular angle
+	dist=Counter(dist_l).most_common()	
+	if dist > lim:	#max distance at 250cm
+		dist = lim
+	
+	print("\""+str(a_ang)+"\":" + str(dist) + ",")
+	
 	
