b88cb34080119c06f845bfca49aedbe49faff219
ksuscella <ksuscella@gmail.com>
2017-06-28 11:17:57 -0400

    line sensor

---
+++ droid.py
@@ -0,0 +1,67 @@
+import urllib
+import urllib2
+import json
+import time
+import logging
+ 
+def main():
+   
+    status = getStatus()
+   
+    if status == 'gopi':
+        # Execute robot command
+        print("execute robot")
+        normal()    #return back to waiting
+        main()      #recursive method
+    if status == 'nope':
+        time.sleep(5)   # Delay for 1 minute (60 seconds). (JUST WAITING AROUND)
+        main()
+    if status == 'quit':
+        print "These are not the droids you were looking for"
+def normal():
+    url_path = 'https://bluepen.herokuapp.com/?type=set&getValue=nope'
+        
+    # Send HTTP POST request
+    request = urllib2.Request(url_path)
+   
+    #Temp code - proxy
+    #proxy = urllib2.ProxyHandler({'https': 'go-proxy.fpl.com:8080'})
+    #opener = urllib2.build_opener(proxy)
+    #urllib2.install_opener(opener)
+    #Temp code - proxy
+       
+    response = urllib2.urlopen(request)
+    parsed_json = json.load(response)
+   
+    #droid_status = parsed_json['status'];   
+    print "return to normal"
+def getStatus():
+    #module that calls a web service to see if its time to move forward
+    #https://bluepen.herokuapp.com/?type=get
+    #https://bluepen.herokuapp.com/?type=set&getValue=gopi
+    #python -m pip install SomePackage --proxy go-proxy.fpl.com:8080
+    url_path = 'https://bluepen.herokuapp.com/?type=get'
+    
+    # page id query
+    query_args = { 'type':'get'}
+   
+    # urlencode data (need urllib)
+    data = urllib.urlencode(query_args)
+       
+    # Send HTTP POST request
+    request = urllib2.Request(url_path)
+   
+    #Temp code - proxy
+    #proxy = urllib2.ProxyHandler({'https': 'go-proxy.fpl.com:8080'})
+    #opener = urllib2.build_opener(proxy)
+    #urllib2.install_opener(opener)
+    #Temp code - proxy
+       
+    response = urllib2.urlopen(request)
+    parsed_json = json.load(response)
+   
+    droid_status = parsed_json['status'];
+    print droid_status
+    return droid_status
+ 
+main()
